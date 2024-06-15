e99cbf0af0ac7b0bd212fff163f9e8d94c0380db
vincentvibe3 <vincentvibe3@gmail.com>
2024-06-15 14:17:59 -0400

    remove limits

--- setup-cluster.sh
+++ setup-cluster.sh
@@ -3,9 +3,6 @@ sudo usermod -a -G microk8s $USER
 mkdir -p ~/.kube
 chmod 0700 ~/.kube
 
-microk8s enable dns
-microk8s enable registry
-
 sudo firewall-cmd --permanent --add-port=6443/tcp #apiserver
 sudo firewall-cmd --permanent --zone=trusted --add-source=10.42.0.0/16 #pods
 sudo firewall-cmd --permanent --zone=trusted --add-source=10.43.0.0/16 #services
