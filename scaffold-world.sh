69c04989b1b301a3f28bf98120eff22f4165dbbd
vincentvibe3 <vincentvibe3@gmail.com>
2024-06-15 14:25:26 -0400

    update scaffold

--- scaffold-world.sh
+++ scaffold-world.sh
@@ -1,3 +1,5 @@
 mkdir -p ~/worlds/$1/backup
-mkdir -p ~/worlds/$1/bluemap-config
+mkdir -p ~/worlds/$1/bluemap-config/config
+mkdir -p ~/worlds/$1/bluemap-config/data
+mkdir -p ~/worlds/$1/bluemap-config/web
 mkdir -p ~/worlds/$1/server
